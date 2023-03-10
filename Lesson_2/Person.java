278e2689cdf93d6b462c0cf7ead4ee9da2ccc6f2
Черкунов Алексей Юрьевич <acherkunov@it-solutions.su>
2023-03-10 15:20:56 +0300

    implementation Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    String gender = "Мужской";
+    String name = "Иван";
+    float height = 1.8f;
+    float weight = 82.5f;
+    int age = 35;
+
+    void go() {
+        System.out.println("Идти");
+    }
+    void sit() {
+        System.out.println("Сидеть");
+    }
+    void run() {
+        System.out.println("Бежать");
+    }
+    void speak() {
+        System.out.println("Говорить");
+    }
+    void learnJava() {
+        System.out.println("Учить Java");
+    }
+}
